e300d2f6b6c3ec32c9b05ef913ee255aafe34723
Matuco19 <96240810+Matuco19@users.noreply.github.com>
2023-12-27 09:14:26 -0300

    first version
    add all

---
+++ README.md
@@ -0,0 +1,29 @@
+<center> <img scr="Images/logo.png" /> </center>
+[![HitCount](https://hits.dwyl.com/Matuco19/yt-vad.svg)](https://hits.dwyl.com/Matuco19/yt-vad)
+
+
+<h2> YT-VAD (YT Video/Audio Downloader) By Matuco19  </h2>
+
+<h3> About the project </h3>
+
+This is a basic backend project of a YouTube video and audio downloader created in Python for learning. 
+
+I used the following modules: 
+
+```
+- pytube
+```
+
+<h3> Roadmap </h3>
+
+-[x]Add video support
+-[x]Add audio support
+-[ ]Add UI
+
+<h3> Screenshots </h3>
+
+<img scr="images/ytvadScreenshot.png" />
+
+
+
+&copy; All Rights Reserved to Matuco19.
---
+++ main.py
@@ -0,0 +1,31 @@
+from pytube import YouTube
+#code by Matuco19 Studios
+print("All rights reserved to Matuco19 Studios.")
+
+def download_media(url, output_path='downloads', media_type='audio'):
+    try:
+      
+        yt = YouTube(url)
+
+        if media_type == 'audio':
+            
+            media_stream = yt.streams.filter(only_audio=True).first()
+        elif media_type == 'video':
+         
+            media_stream = yt.streams.filter(only_video=True).first()
+        else:
+            print("not supported format, using audio type.")
+            media_stream = yt.streams.filter(only_audio=True).first()
+            return
+
+        
+        print(f"Downloading {media_type}: {yt.title}")
+        media_stream.download(output_path)
+        print("Finished Download! Content moved to 'downloads' folder.")
+
+    except Exception as e:
+        print(f"Shit! There is an error: {e}")
+
+
+url = input("Put your URL: ")
+download_media(url, media_type=input("Video or audio(video/audio): "))
